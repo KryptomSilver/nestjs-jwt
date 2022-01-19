515bdc6ff64596bc47c4470bd6e7a4b04647de8b
KryptomSilver <abelromeror763@gmail.com>
2022-01-18 22:55:28 -0600

    Create endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('local/signup')
+  sigupLocal(@Body() dto: AuthDto) {
+    return this.authService.sigupLocal(dto);
+  }
+  @Post('local/sigin')
+  siginLocal() {
+    return this.authService.siginLocal();
+  }
+  @Post('logout')
+  logout() {
+    return this.authService.logout();
+  }
+  @Post('refresh')
+  refreshTokens() {
+    return this.authService.refreshTokens();
+  }
+}
