193ed96e94e637b4ec1b14784428f71eaf46c4ac
KryptomSilver <abelromeror763@gmail.com>
2022-01-18 23:16:34 -0600

    Added type tokens

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
+import { Tokens } from './types';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('local/signup')
-  sigupLocal(@Body() dto: AuthDto) {
+  sigupLocal(@Body() dto: AuthDto): Promise<Tokens> {
     return this.authService.sigupLocal(dto);
   }
   @Post('local/sigin')
